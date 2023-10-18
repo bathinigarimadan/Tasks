d897aa57ac58105c7f0bc83fd547d836e43762cc
bathinigarimadan <148369727+bathinigarimadan@users.noreply.github.com>
2023-10-19 00:16:25 +0530

    Add files via upload

---
+++ CloudVandanaTasks/JavaFiles/Pangram.java
@@ -0,0 +1,28 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Pangram {
+    public static boolean isPangram(String s) {
+        s = s.toLowerCase();
+        Set<Character> uniqueChar = new HashSet<>();
+
+        for (Character ch : s.toCharArray()) {
+            if (ch >= 'a' && ch <= 'z') {
+                uniqueChar.add(ch);
+            }
+        }
+
+        return uniqueChar.size() == 26;
+        
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        boolean res = isPangram(s);
+        System.out.println((res) ? "True" : "False");
+        sc.close();
+    }
+      
+}
